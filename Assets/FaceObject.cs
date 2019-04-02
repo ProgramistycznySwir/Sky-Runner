d7b9b52e85b564a2034a2fdbba6f46cbba687458
ProgramistycznySwir <39069042+ProgramistycznySwir@users.noreply.github.com>
2019-04-02 22:40:14 +0200

    version 0.2.3
    pfffffff....

---
+++ Assets/FaceObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceObject : MonoBehaviour
+{
+    public bool invert;
+    public Transform objectToFace;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(objectToFace, transform.GetComponentInParent<Transform>().up);
+
+        transform.LookAt(objectToFace);
+
+        if (invert) transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+    }
+}
