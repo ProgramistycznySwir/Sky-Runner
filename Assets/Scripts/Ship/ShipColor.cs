1d979b0c2c69b9aecf02f938f0b3845e04d6ec8f
ProgramistycznySwir <39069042+ProgramistycznySwir@users.noreply.github.com>
2019-10-04 15:05:55 +0200

    version 0.3.1.1
    cggh

---
+++ Assets/Scripts/Ship/ShipColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipColor : MonoBehaviour
+{
+    public Color shipColor;
+    private float hue; //takes hue out of color so the ship could be dark and HUD is still visible
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
