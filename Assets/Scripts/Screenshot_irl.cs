a393fcaf61ab71f9f57e9859e805741fdc17de56
ProgramistycznyŚwir <programistycznyswir@gmail.com>
2021-06-23 22:11:09 +0200

    Uploading presentatnion

---
+++ Assets/Scripts/Screenshot_irl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Screenshot_irl : MonoBehaviour
+{
+    public KeyCode takeScreenshot = KeyCode.SysReq;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(takeScreenshot))
+        {
+            string date = System.DateTime.Now.ToString();
+            date = date.Replace("/", "-");
+            date = date.Replace(":", "-");
+
+            string fileName = Application.persistentDataPath + "/Screenshots/screenshot_" + date + ".png";
+            if(!System.IO.Directory.Exists(Application.persistentDataPath + "/Screenshots"))
+                System.IO.Directory.CreateDirectory(Application.persistentDataPath + "/Screenshots");
+
+            try{
+                ScreenCapture.CaptureScreenshot(fileName);
+            }
+            catch(System.Exception e)
+            {
+                Debug.LogError(e);
+                return;
+            }
+
+            Debug.Log("Screenshot at: " + fileName);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Sky-Runner
 
 Just endless runner game i'm developing and optimising as hobby
+
+
+## Screenshots:
+![](Presentation/WallValley.png)
+![](Presentation/ChalkMines1.png)
+![](Presentation/ChalkMines2.png)
